e4ea5a637d6b64d53a79de084d6041dd6b4af7aa
No767 <73260931+No767@users.noreply.github.com>
2022-12-02 22:46:45 -0800

    Upload Files

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+  import Card from './lib/Card.svelte'
+</script>
+
+<main>
+  <div class="grid h-screen place-items-center">
+    <Card/>
+  </div> 
+</main>
---
+++ src/lib/Card.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { Card, ToolbarButton, Dropdown, DropdownItem, Avatar, Button } from "flowbite-svelte";
+</script>
+
+<Card padding='sm' size="lg">
+    <div class="flex flex-col items-center pb-4">
+      <Avatar size="lg" src="/images/profile-picture-3.webp" />
+        <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
+        <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+        <div class="flex mt-4 space-x-3 lg:mt-6">
+          <Button>Add friend</Button>
+          <Button color="light" class="dark:text-white">Message</Button>
+        </div>
+    </div>
+  </Card>
